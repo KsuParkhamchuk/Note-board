d28c413daf53b565ec2b1019bd9fa496ad1c05e2
Ксения Пархомчук <k.parkhamchuk@gmail.com>
2021-03-30 00:41:30 +0300

    add swagger documentetion and enable CORS

--- note-board-server/src/modules/noteBoard/controllers/note.controller.ts
+++ note-board-server/src/modules/noteBoard/controllers/note.controller.ts
@@ -5,36 +5,69 @@ import {
   Get,
   HttpException,
   HttpStatus,
+  NotFoundException,
   Param,
   Post,
   Put,
 } from '@nestjs/common';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Note } from '../entities/note.entity';
 import { NoteService } from '../services/note.service';
 import { CreateDto, UpdateDto } from './dto';
+import { NotFoundResponse } from './type';
 
+@ApiTags('note')
 @Controller('rest/noteboard')
 export class NoteController {
   constructor(private readonly noteService: NoteService) {}
 
+  
   @Get()
+  @ApiResponse( {
+    status: 200,
+    description: 'Get all notes',
+    type: [Note]
+  })
+  @ApiResponse( {
+    status: 404,
+    description: 'Not found',
+    type: NotFoundResponse
+  })
   getAllNotes(): Promise<Note[]> {
     return this.noteService.findAll();
   }
 
   @Get(':id')
+  @ApiResponse( {
+    status: 200,
+    description: 'Get note by id',
+    type: Note
+   })
+   @ApiResponse( {
+    status: 404,
+    description: 'Not found',
+    type: NotFoundResponse
+  })
   async getNoteById(@Param('id') id: string): Promise<Note> {
-    const note = this.noteService.findOne(id);
+    const note = await this.noteService.findOne(id);
     if (note === undefined) {
-      throw new HttpException(
-        'Note with id = ' + id + ' does not exist',
-        HttpStatus.NOT_FOUND,
-      );
+      throw new NotFoundException('Note with id = ' + id + ' does not exists')
     }
     return note;
   }
 
   @Post()
+  @ApiResponse( {
+    status: 200,
+    description: 'Create note',
+    type: Note
+  })
+  @ApiResponse( {
+    status: 404,
+    description: 'Not found',
+    type: NotFoundResponse
+  })
+  @ApiBody({ type: CreateDto })
   createNote(@Body() createDto: CreateDto): Promise<Note> {
     const note = new Note();
     note.title = createDto.title;
@@ -45,16 +78,24 @@ export class NoteController {
   }
 
   @Put(':id')
+  @ApiResponse( {
+    status: 200,
+    description: 'Update note',
+    type: Note
+  })
+  @ApiResponse( {
+    status: 404,
+    description: 'Not found',
+    type: NotFoundResponse
+  })
+  @ApiBody({ type : UpdateDto })
   async updateNote(
     @Param('id') id: string,
     @Body() { title, isCompleted = false }: UpdateDto,
   ): Promise<Note | { error: boolean }> {
     const note = await this.noteService.findOne(id);
     if (note === undefined) {
-      throw new HttpException(
-        'Note with id = ' + id + ' does not exist',
-        HttpStatus.NOT_FOUND,
-      );
+      throw new NotFoundException('Note with id = ' + id + ' does not exists')
     }
     note.title = title;
     note.isCompleted = isCompleted;
@@ -62,6 +103,10 @@ export class NoteController {
   }
 
   @Delete(':id')
+  @ApiResponse( {
+    status: 200,
+    description: 'Detete note',
+  })
   deleteNote(@Param('id') id: string): Promise<void> {
     return this.noteService.remove(id);
   }
